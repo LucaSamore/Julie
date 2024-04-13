73189b8b651bfe614bc313f4bb19f710bd0dd07e
LucaSamore <luca.samore.01@gmail.com>
2024-04-13 10:10:28 +0200

    Add test for username validation

---
+++ data/src/test/java/com/example/data/TestUsername.kt
@@ -0,0 +1,32 @@
+package com.example.data
+
+import com.example.data.user.Username
+import org.junit.Test
+
+class TestUsername {
+
+    @Test
+    fun `Test username is empty`() {
+        assert(Username("").isLeft())
+    }
+
+    @Test
+    fun `Test username contains only white spaces`() {
+        assert(Username("       ").isLeft())
+    }
+
+    @Test
+    fun `Test username is too short`() {
+        assert(Username("a").isLeft())
+    }
+
+    @Test
+    fun `Test username is too long`() {
+        assert(Username("abcdefghi").isLeft())
+    }
+
+    @Test
+    fun `Test username is valid`() {
+        assert(Username("test").isRight())
+    }
+}
