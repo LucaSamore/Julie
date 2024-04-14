9588009a5e3a7d6097afc39ed7ac5b9d25515a63
LucaSamore <luca.samore.01@gmail.com>
2024-04-14 17:08:24 +0200

    Better date and datetime formatting

--- data/src/main/java/com/example/data/user/implementation/FirestoreUserDto.kt
+++ data/src/main/java/com/example/data/user/implementation/FirestoreUserDto.kt
@@ -5,19 +5,20 @@ import com.example.data.user.Interest
 import com.example.data.user.UserProfile
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 internal data class FirestoreUserDto(
     val id: String? = null,
     val firstName: String? = null,
     val lastName: String? = null,
-    val birthDate: LocalDate? = null,
+    val birthDate: String? = null,
     val username: String? = null,
     val emailAddress: String? = null,
     val password: String? = null,
     val interest: List<FirestoreInterestDto>? = null,
     val points: Int? = null,
     val currentStreak: FirestoreCurrentStreakDto? = null,
-    val createdAt: LocalDateTime? = LocalDateTime.now()
+    val createdAt: String? = LocalDateTime.now().betterFormat()
 ) {
     companion object {
         const val COLLECTION = "users"
@@ -27,7 +28,7 @@ internal data class FirestoreUserDto(
                 id = user.id.userId,
                 firstName = user.userDetails.firstName.firstName,
                 lastName = user.userDetails.lastName.lastName,
-                birthDate = user.userDetails.birthDate.birthDate,
+                birthDate = user.userDetails.birthDate.birthDate.betterFormat(),
                 username = user.userDetails.username.username,
                 emailAddress = user.userDetails.emailAddress.emailAddress,
                 password = user.userDetails.password.password,
@@ -40,15 +41,15 @@ internal data class FirestoreUserDto(
 
 internal data class FirestoreCurrentStreakDto(
     val value: Int? = null,
-    val started: LocalDate? = null,
-    val ended: LocalDate? = null
+    val started: String? = null,
+    val ended: String? = null
 ) {
     companion object {
         fun fromEntity(currentStreak: Streak): FirestoreCurrentStreakDto =
             FirestoreCurrentStreakDto(
                 value = currentStreak.value.value,
-                started = currentStreak.begin.value,
-                ended = currentStreak.end.value
+                started = currentStreak.begin.value.betterFormat(),
+                ended = currentStreak.end.value?.betterFormat()
             )
     }
 }
@@ -59,3 +60,9 @@ internal data class FirestoreInterestDto(val name: String? = null, val category:
             FirestoreInterestDto(name = interest.name.name, category = interest.category.category)
     }
 }
+
+private fun LocalDate.betterFormat(pattern: String = "yyyy-MM-dd"): String =
+    format(DateTimeFormatter.ofPattern(pattern))
+
+private fun LocalDateTime.betterFormat(pattern: String = "yyyy-MM-dd"): String =
+    format(DateTimeFormatter.ofPattern(pattern))
